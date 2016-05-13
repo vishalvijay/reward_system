6dc1d71d9c9e8cc9a4132298b4e07e144b60aa6e
Vishal Vijay <0vishalvijay0@gmail.com>
2016-05-13 15:27:00 +0530

    Add sign in user to rspec

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Main", type: :request do
   describe "GET /" do
     it "works! (now write some real specs)" do
+      sign_in_as_a_valid_user
       get root_path
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
   end
 end
